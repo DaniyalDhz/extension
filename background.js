299a434398aed24f8c7a3912c2a14c528393df48
daniyaldhz <daniyaldehleh@gmail.com>
2021-02-04 17:38:46 -0500

    'made code clearer'

--- background.js
+++ background.js
@@ -1,39 +1,35 @@
 var clock = $('.clock').FlipClock(0, {
 	clockFace: 'HourlyCounter',
-	countdown: false });
-	
-function start(){
-	// clock = $('.clock').FlipClock(0, {
-	// clockFace: 'HourlyCounter',
-	// countdown: false });
-	// countup;	
-	// document.location.reload();
-	document.querySelector('#start').innerHTML = 'Extend';
-	document.querySelector('#start').onclick = extend();
+	countdown: false
+}); //initiates Flipclock from jquery
+
+function start() {
+	//should start the timer
+	document.querySelector('#start').innerHTML = 'Extend'; //start button naming should be changed to Exted
 }
 
-function submit(){
-	document.querySelector('#start').innerHTML = 'Start';
-	clock.stop();
-	}
-let time = 2;
+function submit() {
+	document.querySelector('#start').innerHTML = 'Start'; //once submit is hit Extend button should be renamed back to Start
+	clock.stop(); //stops the clock
+}
+let timespan = 2; //in minutes
 
-function extend(){
-	clock = $('.clock').FlipClock(time, {
+function extend() {
+	//should extend the timer from the timespan that it stopped
+	clock = $('.clock').FlipClock(timespan, {
 		clockFace: 'HourlyCounter',
-		countdown: false });
-	}
-
-let countup = setInterval(function () { 
-	if(clock.getTime().time > time) { 
-	clock.stop();
-	clearInterval(countup);
-	}
-	else{
-	var element = document.getElementById("stop");
-	element.addEventListener('click', function(){
-	submit();
+		countdown: false
 	});
 }
-}); 
 
+let countup = setInterval(function () {
+	if (clock.getTime().time > timespan) { //clock.get time starts counting from 0
+		clock.stop();
+		clearInterval(countup); //resets timer
+	} else {
+		var element = document.getElementById("stop");
+		element.addEventListener('click', function () {
+			submit(); //submit also means stop the timer
+		});
+	}
+});
