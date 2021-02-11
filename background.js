2c869ef036c6893fbb2aad7bda39d00ed9949dea
Daniyal <58639693+DaniyalDhz@users.noreply.github.com>
2021-02-11 00:03:07 -0500

    Update background.js

--- background.js
+++ background.js
@@ -33,3 +33,15 @@ let countup = setInterval(function () {
 		});
 	}
 });
+
+fetch('http://127.0.0.1:5000/current', {
+    method: 'POST',
+    body: JSON.stringify({
+        email: '@gmail.com', //sample data
+        id: '2'//sample data
+    }),
+    headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        Accept: 'application/json'
+    }
+})
