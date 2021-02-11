33205165633e72cc18a60a4ec698a99f7e325fbc
daniyaldhz <daniyaldehleh@gmail.com>
2021-02-11 13:13:42 -0500

    'added'

--- background.js
+++ background.js
@@ -1,44 +1,11 @@
-// var countup;
-
-// var _clock = $('.clock').FlipClock(0, {
-// 	clockFace: 'HourlyCounter',
-// 	countdown: false,
-// 	autoStart: false
-// }); //initiates Flipclock from jquery
-
-// var element = document.getElementById("start");
-// element.addEventListener('click', function () {
-// 	document.querySelector('#start').innerHTML = 'Extend'; 
-// 	_clock.start();
-// });
-
-// var element = document.getElementById("stop");
-// element.addEventListener('click', function () {
-// 	var txt = document.querySelector('#enter').innerText;
-// 	if(txt.length > 2)
-// 	{
-// 		document.querySelector('#start').innerHTML = 'Start';
-// 		_clock.stop();
-// 	}
-	
-// });
-
-// let timespan = 2; //in minutes
-
-// function extend() {
-// 	//should extend the timer from the timespan that it stopped
-// 	_clock = $('.clock').FlipClock(timespan, {
-// 		clockFace: 'HourlyCounter',
-// 		countdown: false
-// 	});
-// }
-
-// // countup = setInterval(function () {
-// // 	if (_clock.getTime().time > timespan) { //clock.get time starts counting from 0
-// // 		_clock.stop();
-// // 		clearInterval(countup); //resets timer
-// // 	} else {
-		
-// // 	}
-// // });
-
+fetch('http://127.0.0.1:5000/current', {
+    method: 'POST',
+    body: JSON.stringify({
+        email: '@gmail.com', //sample data
+        id: '2'//sample data
+    }),
+    headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        Accept: 'application/json'
+    }
+})
